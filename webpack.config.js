1034bc60ced8f95b2b34f46c3b84acf47964fdb6
svetlana978 <cbeta279@gmail.com>
2021-07-21 13:31:43 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const PATHS = {
+    source: path.join(__dirname, 'source'),
+    build: path.join(__dirname, 'build')
+};
+module.exports = {
+    entry: PATHS.source + '/index.js',
+    output: {
+        path: PATHS.build,
+        filename: '[name].js'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: "index.html",
+            template: PATHS.source + '/index.pug',
+            //title: 'Webpack app',
+        })
+    ],
+    module: {
+        rules: [{
+            test: /\.pug$/,
+            loader: "pug-loader",
+            options: {
+                pretty: true
+            }
+        }]
+    }
+};
